faea1308a72ed8d534049fb37a7f8bace0b53d38
David Westreicher <david.westreicher@gmail.com>
2024-02-23 00:50:14 +0100

    Fix tests

--- tests/test_cpu.py
+++ tests/test_cpu.py
@@ -1,18 +1,24 @@
+from unittest.mock import Mock
+
+import numpy as np
 import pytest
+
 from chip8.cpu import CPU
-from chip8.display import Display
-from chip8.memory import Memory
 
 
-@pytest.fixture
+@pytest.fixture()
 def cpu() -> CPU:
-    memory = Memory()
-    display = Display()
-    cpu = CPU(memory, display)
-    return cpu
+    memory = Mock()
+    display = Mock()
+    sound = Mock()
+    return CPU(memory, display, sound)
+
+
+def convert_hex_to_uint16(hex_val: str):
+    return np.frombuffer(bytearray.fromhex(hex_val), dtype=np.uint16).byteswap()[0]
 
 
 @pytest.mark.parametrize("register", "0123456789ABCDE")
 def test_instruction_6xnn(cpu: CPU, register: str):
-    cpu.execute(bytearray.fromhex(f"6{register}2A"))
+    cpu.execute(convert_hex_to_uint16(f"6{register}2A"))
     assert cpu.data_registers[int(register, 16)] == 42
